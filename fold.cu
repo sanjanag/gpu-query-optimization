14830afedd15298ef1a74719dbdc48d150f4f926
sanjana <sanjana@cse.iitk.ac.in>
2017-02-06 12:22:59 +0530

    modified fold operation

--- fold.cu
+++ fold.cu
@@ -11,14 +11,26 @@ using namespace std;
 //#define N 1000
 
 __global__ void foldkernel(bool* a, bool*c, int m, int n){
-	int j=blockIdx.x*blockDim.x+ threadIdx.x;
-
-	if(j<n){
-		for(int i=0;i<m;i++)
-			c[j] |= a[i*n+j];
-	}	
+	int threadid = blockIdx.x*blockDim.x+ threadIdx.x;
+	int i = (blockIdx.x*blockDim.x+ threadIdx.x)*4;
+	
+	for(int k=0; k<4; k++){
+		if(i+k >= m)
+			break;
+		for(int j=0; j<n;j++ ){
+			c[threadid*n+j] |= a[(i+k)*n+j];
+		}
+	}
 }
 
+void print(bool* a, int m, int n){
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++)
+			cout << a[i*n+j] << '\t';
+		cout << endl;
+	}
+	cout << endl;
+}
 int main(int argc, char* argv[]){
 	int M,N;
 	M = N = atoi(argv[2]);
@@ -31,42 +43,57 @@ int main(int argc, char* argv[]){
 	long long int size= sizeof(bool) * M * N;
 	double gpu_time_used,cpu_time_used;
 
-
 	bool* mat = (bool*)malloc(size);
-	bool* res = (bool*)malloc(sizeof(bool)*N);
+	bool* res = (bool*)malloc(sizeof(bool)*((M+4-1)/4)*N);
+	bool* mask = (bool*)malloc(sizeof(bool)*N);
+
 	bool *d_mat,*d_res;
-	int threadsPerBlock = 512;
-	long long int numBlocks = (N+threadsPerBlock-1)/threadsPerBlock;
+	int threadsPerBlock = 10;
+	long long int numBlocks = (M+threadsPerBlock-1)/threadsPerBlock;
+
 	for(long long int i=0; i<M; i++){
 		for(long long int j=0; j<N; j++)
 			in >> mat[i*N+j];
 	}
-	for(long long int i=0;i<N;i++)
-		res[i]=false;
 
-	
-	
+	for(long long int i=0;i<((M+4-1)/4);i++)
+		for(int j=0;j<N;j++)
+			res[i*N+j]=false; 
+	for(int i=0;i<N;i++)
+		mask[i]=false;	
+
+	//print(mat, M, N);
 	start=clock();
+
 	cudaMalloc((void**)&d_mat, size);
-	cudaMalloc((void**)&d_res, sizeof(bool)*N);
+	cudaMalloc((void**)&d_res, sizeof(bool)*((M+4-1)/4)*N);
 	cudaMemcpy(d_mat, mat, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_res, res, sizeof(bool)*N, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_res, res, sizeof(bool)*((M+4-1)/4)*N, cudaMemcpyHostToDevice);
+
 	for(int k=0;k<iter;k++)
 		foldkernel<<<numBlocks, threadsPerBlock>>>(d_mat, d_res,M,N);
-	cudaMemcpy(res, d_res, sizeof(bool)*N, cudaMemcpyDeviceToHost);	
-	end = clock();
-	gpu_time_used = ((double)(end-start))/CLOCKS_PER_SEC;
-//	cout << "GPU: " << gpu_time_used << endl;
 
-	start=clock();
+	cudaMemcpy(res, d_res, sizeof(bool)*((M+4-1)/4)*N, cudaMemcpyDeviceToHost);	
 	for(int k=0;k<iter;k++){
-		for(int j=0; j<N; j++){
-			for(int i=0; i<M; i++)
-				res[j] |= mat[i*N+j];
+		for(int i=0;i<((M+4-1)/4);i++){
+			for(int j=0;j<N;j++)
+				mask[j] |= res[i*N+j];
 		}
 	}	
 	end = clock();
-	
+	//print(res,((M+4-1)/4),N);
+	//print(mask,1,N);
+	gpu_time_used = ((double)(end-start))/CLOCKS_PER_SEC;
+//	cout << "GPU: " << gpu_time_used << endl;
+	start=clock();
+	for(int k=0;k<iter;k++){
+		for(int i=0;i<M;i++){
+			for(int j=0;j<N;j++)
+				mask[j] |= mat[i*N+j];
+		}
+	}	
+	end = clock();
+	//print(mask,1,N);	
 	cpu_time_used = ((double)(end-start))/CLOCKS_PER_SEC;
 //	cout << "CPU: " << cpu_time_used << endl;
 	cout << "CPU/GPU: " << cpu_time_used/gpu_time_used << endl;	
