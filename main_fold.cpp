e4f0251d2720743b2c7e2c0abf43e27fcc00fa12
sanjana <sanjana@cse.iitk.ac.in>
2017-04-20 07:01:50 +0530

    everything working except for read data

--- main_fold.cpp
+++ main_fold.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include "helper.h"
 #include <stdio.h>
+#include <cuda.h>
+#include <sys/time.h>
 
 using namespace std;
 
@@ -15,25 +17,27 @@ __device__ void Memcpy(void* dest, void* src, size_t n){
 }
 
 
-__global__ void foldkernel(unsigned long long int* mapping, unsigned char* input, unsigned char* res, int n,int size_mask, int split){
-	unsigned int GAP_SIZE_BYTES = sizeof(unsigned int);
-	unsigned int ROW_SIZE_BYTES = sizeof(unsigned int);
+__global__ void foldkernel(int* mapping, unsigned char* input, unsigned char* res, int n,int size_mask, int split){
+	unsigned int gap_size = sizeof(unsigned int);
+	unsigned int row_size = sizeof(unsigned int);
     int threadid = blockIdx.x*blockDim.x+ threadIdx.x;
     int i = threadid*split;
     //int gap_size = sizeof(unsigned int);
     
     for(int k = 0; k < split; k++){
-        int rowid = i+k;
+        unsigned int rowid = i+k;
         if(rowid >= n)
             break;
         if(mapping[rowid]==-1)
             continue;
         unsigned char* data = input+mapping[rowid];
         unsigned rowsize=0;
-        Memcpy(&rowsize, data, ROW_SIZE_BYTES);
-        data += ROW_SIZE_BYTES;
-        unsigned int total_cnt = (rowsize-1)/GAP_SIZE_BYTES;
+        Memcpy(&rowsize, data, row_size);
+        data += row_size;
+        unsigned int total_cnt = (rowsize-1)/gap_size;
         unsigned int cnt = 0;
+                    unsigned int tmpcnt = 0;
+
         unsigned int bitpos = 0, bitcnt = 0;
         bool flag = data[0] & 0x01;
         unsigned char format = data[0] & 0x02;
@@ -41,8 +45,7 @@ __global__ void foldkernel(unsigned long long int* mapping, unsigned char* input
         data++;
 
         while(cnt < total_cnt){
-        	unsigned int tmpcnt = 0;
-        	Memcpy(&tmpcnt, data[cnt*GAP_SIZE_BYTES], GAP_SIZE_BYTES);
+        	Memcpy(&tmpcnt, &data[cnt*gap_size], gap_size);
         	if(format == 0x02){
         		res[threadid*size_mask + (tmpcnt-1)/8] |= (0x80 >> ((tmpcnt-1)%8));
         	}
@@ -69,11 +72,14 @@ __global__ void foldkernel(unsigned long long int* mapping, unsigned char* input
 
 
 int main(){
+    struct timeval t1, t2;
+    double elapsedTime;
+
 	BitMat* bitmat = new BitMat;
 	unsigned int node = 1;
-	char dumpfile[1024] = "/work/scratch/datasets/dbpedia/all_data_bitmats/dbpedia565m_pos_odump";
-	
-	gnum_subs=20;
+	//char dumpfile[1024] = "/work/scratch/datasets/dbpedia/all_data_bitmats/dbpedia565m_pos_odump";
+    char dumpfile[1024] = "/data/gpuuser1/dbpedia565m_pos_odump";	
+	gnum_subs=10;
 	gnum_preds=57453;
 	gnum_objs=153561757;
 	gnum_comm_so=27116793;
@@ -83,17 +89,27 @@ int main(){
 	unsigned int triples =  load_from_dump_file(dumpfile, node, bitmat, true, true, NULL, 0, 0, NULL, 0, true);
 	//print_bitmat(bitmat->bm);
 	//cout << count_size_of_bitmat(bitmat) << endl;	
-	//simple_fold(bitmat, COLUMN, bitmat->objfold, bitmat->object_bytes);
-	
-	unsigned long long int mapping[gnum_subs];
+    gettimeofday(&t1, NULL);
+
+	simple_fold(bitmat, COLUMN, bitmat->objfold, bitmat->object_bytes);
     
+    gettimeofday(&t2, NULL);
+    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+    elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+    cout << elapsedTime << " ms.\n";	
+
+
+	int mapping[gnum_subs];
+    
+    gettimeofday(&t1, NULL);
     for(int i=0; i<gnum_subs; i++)
-        mapping[i] = i;
+        mapping[i] = -1;
 
     unsigned long long int size_gpu_input = get_sizeof_1dimarr(bitmat->bm);
     unsigned char* gpu_input = (unsigned char*)malloc(size_gpu_input*sizeof(unsigned char));
     convert_bitmat_to_1dimarr(bitmat->bm, mapping, gnum_subs, gpu_input);
     int n = gnum_subs;
+    int size_mask = (n%8>0 ? n/8+1 : n/8);
     int split = 8;
     int rows_res = (n+split-1)/split;
     int size_res = rows_res*size_mask;
@@ -135,6 +151,9 @@ int main(){
             gpu_mask[j] |= res[i*size_mask+j];
         }
     }
-	
+	gettimeofday(&t2, NULL);
+    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+    elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+    cout << elapsedTime << " ms.\n";    
 	return 0;
 }
