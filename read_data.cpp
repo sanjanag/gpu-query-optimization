30a9821974aaa38cce604356008fa181e09ec9ae
sanjana <sanjana@cse.iitk.ac.in>
2017-04-25 14:14:08 +0530

    bitmat with s*o dimension loaded

--- read_data.cpp
+++ read_data.cpp
@@ -8,25 +8,16 @@ using namespace std;
 
 int main(){
 	cout << "Hello World\n";
-	//cout << table_col_bytes << endl;
 	BitMat* bitmat = new BitMat;
-	char dumpfile[1024] = "/work/scratch/datasets/dbpedia/all_data_bitmats/dbpedia565m_pos_odump";
+	char dumpfile[1024] = "/work/scratch/datasets/dbpedia/all_data_bitmats/dbpedia565m_spo_pdump";
 
-	init_bitmat(bitmat, gnum_subs, gnum_preds, gnum_objs, gnum_comm_so, POS_BITMAT);
+	init_bitmat(bitmat, gnum_subs, gnum_preds, gnum_objs, gnum_comm_so, SPO_BITMAT);
 
-	for(unsigned int i=1; i <= gnum_objs; i++){
-		cout << "i: " << i <<  endl; 
-		unsigned int triples =  load_from_dump_file(dumpfile, i, bitmat, true, true, NULL, 0, 0, NULL, 0, true);	
-		cout << "size of  bitmat: " <<  count_size_of_bitmat(bitmat) << endl;
-		simple_fold(bitmat, COLUMN, bitmat->objfold, bitmat->object_bytes);
-		clear_rows(bitmat, true, true, false);
-	}
-
+	unsigned int triples =  load_from_dump_file(dumpfile, 1, bitmat, true, true, NULL, 0, 0, NULL, 0, true);
 	
-	/*cout << triples << endl;
-	cout << bitmat->num_subs << '\t' << bitmat->num_preds << '\t' << bitmat->num_objs << '\t' << bitmat->num_comm_so << endl;
-	cout << bitmat->bm.size() << endl;*/
-	/*cout << "size of  bitmat: " <<  count_size_of_bitmat(bitmat) << endl;
-	simple_fold(bitmat, COLUMN, bitmat->objfold, bitmat->object_bytes);*/
+	cout << "1" << " " << bitmat->bm.size() << endl;	
+	
+	simple_fold(bitmat, COLUMN, bitmat->objfold, bitmat->object_bytes);
+	
 	return 0;
 }
